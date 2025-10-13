5af974747933f5643691c04a4eac3d78f4a7f0be
xinmei <yaowenchang3@gmail.com>
2025-10-13 11:46:59 +0800

    modify red module
    modify red module

--- module/red.py
+++ module/red.py
@@ -2,29 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 import json
-from common.requests_common import RequestsCommon
-
-
-user_id = '1180458703'  # 替换为你的用户ID
-max_id = None
-URL_RED = "https://www.xiaohongshu.com/user/profile/5ff94a1d000000000100bf27?xhsshare=CopyLink&appuid=609513cd000000000101e44f&apptime=1681699386"
+from requesets_lib.common.user_info_manager import UserInfo
 
 
 HEADER_RED = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',  # 替换为你的User-Agent
-    'Cookie': 'abRequestId=d73583d2-86f3-5e76-aa0a-6c61bd8330f6; a1=19319b5c06aem8zq1z7vao3nnc0jf0t6x6whei01250000374805; webId=101d894b99c69a3cd926c5ecf0cc2e64; gid=yjqyjD2fyWC2yjqyjD2S8K38K0d6YuAyuWh0EyJlqIIS8x28Vi89Kd888qW4Y82880Y802iD; x-user-id-creator.xiaohongshu.com=61b1675c000000001000e4fb; customerClientId=478402480106079; xsecappid=xhs-pc-web; acw_tc=0a00d62217524201274523713e84ec43eef720f2d9f6c1042087e4847bed57; webBuild=4.72.0; loadts=1752420096742; websectiga=82e85efc5500b609ac1166aaf086ff8aa4261153a448ef0be5b17417e4512f28; sec_poison_id=9f0c09fa-4ce0-43a8-964e-6bc3a4e23b95; web_session=040069b1e1508557b4192587463a4b268b8661',
-    'Referer': 'https://www.xiaohongshu.com/',
-    'Accept': 'application/json, text/plain, */*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0',  # 替换为你的User-Agent
+    'Cookie': 'abRequestId=d73583d2-86f3-5e76-aa0a-6c61bd8330f6; a1=19319b5c06aem8zq1z7vao3nnc0jf0t6x6whei01250000374805; webId=101d894b99c69a3cd926c5ecf0cc2e64; gid=yjqyjD2fyWC2yjqyjD2S8K38K0d6YuAyuWh0EyJlqIIS8x28Vi89Kd888qW4Y82880Y802iD; x-user-id-creator.xiaohongshu.com=61b1675c000000001000e4fb; customerClientId=478402480106079; xsecappid=xhs-pc-web; acw_tc=0a00d5b317592411126147129e703d29825f0b5b76a6b3d8783a02715f77ca; webBuild=4.81.0; loadts=1759241110811; websectiga=3fff3a6f9f07284b62c0f2ebf91a3b10193175c06e4f71492b60e056edcdebb2; sec_poison_id=de2d42a9-81c2-4131-9f9b-35ac2636fca4; web_session=040069b1e1508557b419d8abee3a4bfea11c5b; unread={%22ub%22:%2268d949ca000000001003e277%22%2C%22ue%22:%2268d7af02000000001101d636%22%2C%22uc%22:32}',
+    'Referer': 'https://www.xiaohongshu.com',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6'
 }
 
+user_info = UserInfo.user_info_loader()
+user_info_red = user_info["red"]
+
+URL_RED = user_info_red["url"]
+
+
 def get_red_user_info():
     response = requests.get(URL_RED, headers=HEADER_RED)
+    # response = requests.get(user_info_red["url"], HEADER_RED)
     if response.status_code != 200:
-        print(f"请求失败，状态码: {response.status_code}")
+        print(f"get_red_user_info: 请求失败，状态码: {response.status_code}")
         return None
     if not response.text:
-        print("响应内容为空")
+        print("get_red_user_info: 响应内容为空")
         return None
 
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -35,10 +37,12 @@ def get_red_user_info():
     state_with_script = state.replace('window.__INITIAL_STATE__=', '')
     state_with_script = state_with_script.replace('undefined', 'null')
 
+    # pprint(state_with_script)
+
     try:
         red_user_data = json.loads(state_with_script)
     except json.JSONDecodeError as e:
-        print(f"解析JSON失败: {e}")
+        print(f"get_red_user_info: 解析JSON失败: {e}")
         return None
 
     basicInfo = red_user_data['user']['userPageData']['basicInfo']
@@ -46,29 +50,47 @@ def get_red_user_info():
     user_id = basicInfo['redId'] # 小红书号
     ip_location = basicInfo['ipLocation'] # IP位置
     description = basicInfo['desc']  # 用户简介
-
+    notes = red_user_data['user']['notes']  # 作品数，某些用户可能没有该字段
+    note_count = 0
+    for note in notes:
+        if note:
+            note_count += 1
+        else:
+            break
+    
     interactions = red_user_data['user']['userPageData']['interactions']
     follows = 0
     fans = 0
     interaction = 0
     for item in interactions:
         if item['type'] == 'follows':
-            follows = int(item['count'])
-        elif item['type'] == 'fans':
-            fans = int(item['count'])
-        elif item['type'] == 'interaction':
-            # interaction = item['count']
-            interaction = int(item['count'])
+            try:
+                follows = int(item['count'])
+            except ValueError:
+                follows = item['count']
+        if item['type'] == 'fans':
+            try:
+                fans = int(item['count'])
+            except ValueError:
+                fans = item['count']
+        if item['type'] == 'interaction':
+            try:
+                interaction = int(item['count'])
+            except ValueError:
+                interaction = item['count']
 
     user_info = {
-        '昵称'      : nickname,
+        '昵称'       : nickname,
         '小红书号'   : user_id,
-        'IP属地'    : ip_location,
-        '简介'     : description,
-        '关注数'   : follows,
-        '粉丝数'   : fans,
-        '获赞与收藏': interaction
+        'IP属地'     : ip_location,
+        '简介'       : description,
+        '作品数'     : note_count,
+        '关注数'     : follows,
+        '粉丝数'     : fans,
+        '获赞与收藏' : interaction
     }
+    print("get_red_user_info:", user_info)
+
     return user_info
 
 
