7544efb935825d27537932485d665670c9f7a67b
xinmei <yaowenchang3@gmail.com>
2025-10-14 16:12:30 +0800

    midify database.py
    modify database.py

--- common/database.py
+++ common/database.py
@@ -1,13 +1,17 @@
 import pymysql
 from pprint import pprint
+from requesets_lib.common.user_info_manager import UserInfo
+
+user_info_database = UserInfo.user_info_loader()["database"]
 
 class DataBase:
     connection = pymysql.connect(
-        host='localhost',
-        user='root',
-        password='root',  # 替换为你的数据库密码
-        database='socialharvest',  # 替换为你的数据库名称
-        charset='utf8mb4',
+        host      = user_info_database["host"],
+        port      = user_info_database["port"],
+        user      = user_info_database["user"],
+        password  = user_info_database["password"],
+        database  = user_info_database["database"],
+        charset   = user_info_database["charset"]
     )
     cursor = connection.cursor()
     def __init__(self):
@@ -66,12 +70,12 @@ class DataBase:
         return results, results_dict 
 
     def insert_tiktok(self, data):
-        sql = "INSERT INTO tiktok (昵称, 抖音号, IP属地, 粉丝数, 关注数, 获赞数, 作品数, 喜欢作品数, 签名, 时间) VALUES " + "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        sql = "INSERT INTO tiktok (昵称, 抖音号, IP属地, 粉丝数, 关注数, 获赞数, 作品数, 总评论数, 喜欢作品数, 签名, 时间) VALUES " + "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
         self.cursor.execute(sql, data)
         self.connection.commit()  # 提交事务
     
     def insert_tiktok_video(self,data):
-        sql = "INSERT INTO tiktok_video (视频_id, 标题, 评论数, 点赞数, 分享数, 创建时间, 视频地址) VALUES " + "(%s, %s, %s, %s, %s, %s, %s)"
+        sql = "INSERT INTO tiktok_video (视频_id, 标题, 评论数, 点赞数, 分享数, 创建时间, 视频地址, 时间戳) VALUES " + "(%s, %s, %s, %s, %s, %s, %s, %s)"
         self.cursor.execute(sql, data)
         self.connection.commit()
 
@@ -90,20 +94,19 @@ class DataBase:
         self.cursor.execute(sql, (video_id,))
         count = self.cursor.fetchone()[0]
         return count > 0
+    
+    def search_video_by_id(self, video_id):
+        sql = "SELECT * FROM tiktok_video WHERE 视频_id = %s order by id desc LIMIT 1"
+        self.cursor.execute(sql, (video_id,))
+        result = self.cursor.fetchone()
+        results_dict = DataBase.__return_diction(self, result)  # 将结果转换为字典形式
+        return results_dict
 
 
 if __name__ == "__main__":
         # 测试代码
     database = DataBase()  # 创建数据库连接实例
-    # for item in database.search_common("select * from weibo_fans"):
-    #     # tuple_in = (item[1], item[2], item[3][-10:])
-    #     # print(item[1], item[2], item[3][-10:])
-    #     command = "update weibo_fans set 微博号={} where 昵称 = '{}'".format(item[3][-10:], item[1])
-    #     database.insert_common(command)
-    # last_row, last_row_dict = database.search_tiktok()  # 获取最后一行数据
-    # new_tuple = last_row[1:-1]
-    # tuple_data = ('118', '46', '75', '66', '105', '81', 'example_content')  # 示例数据
-    # database.insert_tiktok(tuple_data)  # 插入数据
+    pprint(database.search_common("select video_id from tiktok"))
     # database.insert_weibo(tuple_data)  # 插入数据
     last_row = ( "趣多多", "1180458703", "ip", "简介", 0, 16, 3, 0, "2025-06-24 22:45:00")  # 示例数据
     # database.insert_redbook(last_row)  # 插入数据
